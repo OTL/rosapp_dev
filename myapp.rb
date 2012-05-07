c828f3f0c2dbd947c7f478e54e8734630d99c33a
Takashi Ogura <t.ogura@gmail.com>
2012-05-08 01:35:37 +0900

    success subscription

--- myapp.rb
+++ myapp.rb
@@ -18,43 +18,51 @@ class MyApp < Sinatra::Base
     @menu = ['node', 'topic', 'service']
     @active = 'node'
     @sockets = []
+
+    @node.subscribe('/rosout_agg', Rosgraph_msgs::Log) do |msg|
+      @sockets.each do |ws|
+        case msg.level
+        when 2
+          level = 'alert-info'
+        when 4
+          level = ''
+        when 8
+          level = 'alert-error'
+        else
+          level = 'alert-error'
+        end
+        ws.send("<div class=\"alert #{level}\"><button class=\"close\" data-dismiss=\"alert\">x</button>#{msg.msg}</div>")
+      end
+    end
+
+    sleep(1.0)
+    p 'hoge'
+    t = Thread.new do
+      p 'start'
+      while @node.ok?
+        @node.spin_once
+        sleep(0.1)
+      end
+      p 'end'
+    end
+
     EventMachine::WebSocket.start(:host => "localhost", :port => 8080) do |ws|
       ws.onopen do
-	ws.send "Hello Client!"
-	p 'hello!!!'
-	@sockets << ws
+        @sockets << ws
       end
       ws.onmessage { |msg| ws.send "Pong: #{msg}" }
       ws.onclose   { puts "WebSocket closed" }
     end
 
-    @node.subscribe('/rosout', Rosgraph_msgs::Log) do |msg|
-      p 'msg come!'
-      p @sockets
-      @sockets.each do |ws|
-	p 'msg come!'
-	ws.send(msg.msg)
-      end
-    end
-    sleep(1.0)
-    p 'hoge'
-    t = Thread.new do
-      while @node.ok
-	p 'spin1'
-	@node.spin_once
-	sleep(0.1)
-	p 'spin2'
-      end
-    end
   end
 
   def print_nav
     output = ''
     @menu.each do |title|
       if title == @active
-	output += "<li class=\"active\"><a href=\"#{title}\">#{title.capitalize}</a></li>\n"
+        output += "<li class=\"active\"><a href=\"#{title}\">#{title.capitalize}</a></li>\n"
       else
-	output += "<li><a href=\"#{title}\">#{title.capitalize}</a></li>\n"
+        output += "<li><a href=\"#{title}\">#{title.capitalize}</a></li>\n"
       end
     end
     output
@@ -68,9 +76,11 @@ class MyApp < Sinatra::Base
     nodes = Set.new
     @master.get_system_state.each do |states|
       states.each do |topic, publishers|
-	publishers.each do |pub|
-	  nodes.add(pub)
-	end
+        publishers.each do |pub|
+          if pub != @node.node_name
+            nodes.add(pub)
+          end
+        end
       end
     end
     nodes.to_a
@@ -80,7 +90,7 @@ class MyApp < Sinatra::Base
     topics = Set.new
     @master.get_system_state[0,2].each do |states|
       states.each do |topic, publishers|
-	topics.add(topic)
+        topics.add(topic)
       end
     end
     topics.to_a
@@ -111,6 +121,5 @@ class MyApp < Sinatra::Base
     erb :service
   end
 end
-Thin::Server.start MyApp, '0.0.0', 9090
+Thin::Server.start MyApp, '0.0.0.0', 9090
 end
-#MyApp.run! :host => 'localhost', :port => 9090
